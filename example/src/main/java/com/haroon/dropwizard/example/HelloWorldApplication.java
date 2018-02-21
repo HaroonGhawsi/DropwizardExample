cbf7f28add7e8b75987bf24e67a2c28e06556c98
Haroon Ghawsi <haroon.ghawsi@frs.de>
2018-02-21 09:28:58 +0100

    Initial Commit

---
+++ example/src/main/java/com/haroon/dropwizard/example/HelloWorldApplication.java
@@ -0,0 +1,33 @@
+package com.haroon.dropwizard.example;
+
+import com.haroon.dropwizard.example.health.TemplateHealthCheck;
+import com.haroon.dropwizard.example.resources.HelloWorldResource;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+	
+	public static void main(String[] args) throws Exception{
+		new HelloWorldApplication().run(args);
+	}
+	
+	@Override
+	public String getName(){
+		return "hello-world";
+	}
+	
+	@Override
+	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap){
+		//Nothing to do yet
+	}
+
+	@Override
+	public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+		final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
+		final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+		environment.healthChecks().register("template", healthCheck);
+		environment.jersey().register(resource);
+	}
+}
